e1638457a942c40ffb5f2faedf9e961114e1bd22
nathangappy <nate.gappy@gmail.com>
2019-09-05 16:08:07 -0400

    initial commit
    initial project setup
    create dashboard component

---
+++ src/components/layout/SignedIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedIn = () => {
+  return (
+    <ul className="right">
+      <li><NavLink to='/'>New Post</NavLink></li>
+      <li><NavLink to='/'>Log Out</NavLink></li>
+      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NG</NavLink></li>
+    </ul>
+  )
+}
+
+export default SignedIn;
